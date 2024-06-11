9c960d025a690149b1c345581b6f0df052f5344f
Drew Wibbenmeyer <drako0812@users.noreply.github.com>
2024-06-11 15:18:44 -0500

    Small changes.

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"unicode"
+)
+
 const SOURCE string = `EXIT 0;`
 
 type TokenT uint
@@ -52,7 +58,80 @@ func NewTokenLitInt(v int32, path string, line, column int64) *TokenLitInt {
 }
 
 type Tokenizer struct {
-	tokens []Token
+	tokens []*Token
+	pos int // The current offset into src
+	line int // The current line inside the src
+	col int // The current column inside the src
+	src string // The source code being read
+}
+
+func NewTokenizerFromString(filename, source string) *Tokenizer {
+	return &Tokenizer {
+		tokens: []*Token{},
+		pos: 0,
+		line: 0,
+		col: 0,
+		src: source,
+	}
+}
+
+func NewTokenizer(filepath string) *Tokenizer {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		fmt.Printf("Error reading file \"%v\": %v\n", filepath, err)
+		return nil
+	}
+
+	return NewTokenizerFromString(filepath, string(data))
+}
+
+func (t *Tokenizer) Advance() error {
+	t.pos++
+	if t.pos >= len(t.src) {
+		return fmt.Errorf("Unexpected end of file")
+	}
+
+	t.col++
+
+	if t.src[t.pos] == '\n' {
+		t.line++
+		t.col = 0
+	}
+
+	return nil
+}
+
+func (t *Tokenizer) AdvanceMulti(n int) (int, error) {
+	var count int = 0
+	for i := 0; i < n; i++ {
+		if err := t.Advance(); err != nil {
+			return count, err
+		}
+		count++
+	}
+	return count, nil
+}
+
+func (t *Tokenizer) Peek(offset int) (rune, error) {
+	if t.pos + offset >= len(t.src) {
+		return 0, fmt.Errorf("Unexpected end of file")
+	}
+
+	return rune(t.src[t.pos+offset]), nil
+}
+
+func (t *Tokenizer) EatWhitespace() error {
+	ch, err := t.Peek(0)
+	for unicode.IsSpace(ch) && err == nil {
+		if err := t.Advance(); err != nil {
+			return err
+		}
+	}
+}
+
+func (t *Tokenizer) Tokenize() []*Token {
+
+	return t.tokens
 }
 
 func main() {
